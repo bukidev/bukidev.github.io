b575429162e5468b3f319f939aa8291345859ca5
bukidev <dilstudio29@gmail.com>
2018-01-23 13:46:12 +0700

    update langpack

--- assets/js/components/langpack/th.js
+++ assets/js/components/langpack/th.js
@@ -1,10 +1,10 @@
-var en = {
-    navbar: ['Home', 'About', 'Experience', 'Work', 'Contact'],
-    name: 'Chainuwat',
-    surname: 'Chuaykontong',
+var th = {
+    navbar: ['หน้าหลัก', 'เกี่ยวกับ', 'ประสบการณ์', 'ผลงาน', 'ติดต่อ'],
+    name: 'ชัยนุวัตร',
+    surname: 'ช่วยคงทอง',
     job: 'Graphic & Web Designer',
-    head1: 'About',
+    head1: 'แนะนำตัว',
     subhead1: '',
     t1: 'ทำไมต้องศัลยกรรมหน้าอกกับ BongBong?',
     t2: 'ศัลยแพทย์เกาหลีของ BongBong มีความเชี่ยวชาญและประสบการณ์สูง เอื้อให้สามารถผ่าตัดแบบ Oneday Breast Surgery หรือ การเสริมหน้าอกแบบเจ็บน้อยจบได้ในเวลาแค่ 1 วัน โดยไม่ต้องนอนพักฟื้นที่โรงพยาบาลข้ามคืน'
-}
+};
