3e7b591ff022dd28083ed9ad418c9e18ca71c53d
bukidev <dilstudio29@gmail.com>
2018-01-23 13:13:59 +0700

    update lang pack"

---
+++ assets/js/components/langpack/en.js
@@ -0,0 +1,101 @@
+var en = {
+    
+    //---------------------------------------------------------------
+    //-----------------------เริ่มหัวข้อ Header---------------------------
+    //---------------------------------------------------------------
+    
+    header: '1 Day Breast Surgery',
+    subhead: 'Minimal incision, Less bleeding, Safe breast plastic surgery',
+    
+    ////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////
+    
+    
+    
+    //---------------------------------------------------------------
+    //-----------------------เริ่มหัวข้อที่สอง------------------------------
+    //---------------------------------------------------------------
+    
+    "t1" : "Why BongBong special breast surgery?",
+    "t2" : "Experienced surgeons provide a one day breast surgery system without overnight hospital admission",
+        
+    ////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////
+        
+        
+        
+    //---------------------------------------------------------------
+    //-----------------------เริ่มหัวข้อที่สอง------------------------------
+    //---------------------------------------------------------------
+        
+    "t3" : "What makes BongBong One day Breast Surgery more special than others",
+    
+    "t4" : "Minimal incision, Less bleeding, Safe breast plastic surgery",
+
+    "t5" : "Cumulative experience makes it possible for minimal bleeding during the surgery. Drainage and compression bands are not needed because the bleeding amout is less than having eyes or nose surgery. These leads to fast recovery and less pain and less side effects after the surgery.",
+        
+    "t6" : "High Technology Checkup/Surgery Devices/Post Operation care system setup",    
+       
+    "t7" : "BongBong Clinic has high quality technological support system that can be compared with top hospitals such as  Breast Ultrasonography, Full HD Advanced Endoscope, Motiva Divana, Mammotomy, Smart Lux",
+        
+    "t8" : "Breast/Body Surgery specialized",
+
+    "t9" : "Generally, breast plastic surgery takes an hour and a half to three hours. But at BongBong, we get it done precisely in a shorter time.",
+        
+    "t10" : "Side effect and complication minimized",
+
+    "t11" : "It is hard to believe if someone says it is not painful after a breast plastic surgery. But at BongBong, we make this impossible for most of the patients. We have managed to bring lower side effects and complications rate",
+        
+    "t12" : "Post operation pain and discomfort minimized",
+
+    "t13" : "We provide less post-operation visits. Day 1, Day 7, 1 month, 3 months, 6 months follow up are all the visits you need. Some patient feel it is unnecessary to return to the clinic for follow up, but it is recommended to check up on your progress",
+        
+    "t14" : "Fast recovery to everyday life",
+
+    "t15" : "BongBong clinic provides minimal incision, less bleeding compared to other clinics, making it possible for faster recovery. Post-operation wound care system is provided for more satisfying results.",
+        
+        ////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////
+        
+        
+        
+        //---------------------------------------------------------------
+        //-----------------------เริ่มหัวข้อที่สาม------------------------------
+        //---------------------------------------------------------------
+        
+    "t16" : "Real story",
+        
+        ////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////
+        
+        
+        
+        //---------------------------------------------------------------
+        //-----------------------เริ่มหัวข้อที่สี่------------------------------
+        //---------------------------------------------------------------
+        
+    "t17" : "BongBong breast plastic surgery safe process",
+        
+    "t18" : "Pre/Post-operative measurement with Motiva Divina calculates and analyzes the expected shape and results.",
+        
+    "t19" : "Individualized surgery plan",
+        
+    "t20" : "After care service for unexpected side effects prevention",
+        
+        ////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////
+        
+        
+        
+        //---------------------------------------------------------------
+        //-----------------------เริ่มหัวข้อที่ห้า------------------------------
+        //---------------------------------------------------------------
+        
+    "t21" : "We take care our patients with sincerity and give our best for your satisfy",
+        
+    "t22" : "Safe and clear surgical preparation by using Smart Lux light sterilization on the patients' surgical area which decreases post-operation infection rate.",
+        
+    "t23" : "Post-operative light therapy for sterilization and enhance regeneration.",
+        
+    "t24" : "Erase the post-operative scar by using scar management technological divice."
+}
