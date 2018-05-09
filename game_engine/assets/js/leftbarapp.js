cd0b50049968bd2e819364db316f60323b026b84
Bukhari29 <dilstudio29@gmail.com>
2018-05-09 22:53:03 +0700

    add phaser

---
+++ game_engine/assets/js/leftbarapp.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#leftBarApp',
+  data: {
+    items: [
+        { text: 'ชื่อโปรเจคท์',
+          description: 'ชื่อของโปรเจ็คที่เราต้องการ'    
+        },
+        { text: 'เวอร์ชั่น'},
+        { text: 'คำบรรยาย'},
+        { text: 'ไอดี'},
+        { text: 'ผู้ผลิต'}
+    ]
+  }
+})
