3bbe2acba4250f5c64f9404fc3187c2aef256f9e
Chainuwat Chuaykongtong <chainuwatr.design@gmail.com>
2018-05-11 22:50:41 +0700

    Update main.js

--- hardcore-pilot/js/main.js
+++ hardcore-pilot/js/main.js
@@ -1,5 +1,9 @@
 var game = new Phaser.Game(config.screenWidth, config.screenHeight, config.render, 'game', {preload: preload, create: create, update: update});
 
+function preload() {};
+function create() {};
+function update() {};
+
 game.state.add('boot', state1);
 game.state.add('intro', state2);
 
