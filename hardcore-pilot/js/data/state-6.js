a5d2e283edd9c704f720154ffe7f5602c13a149a
bukidev <dilstudio29@gmail>
2018-05-12 14:31:05 +0700

    try to add player control

---
+++ hardcore-pilot/js/data/state-6.js
@@ -0,0 +1,43 @@
+var cursor, fireButton, player;
+
+var state6 = {
+    preload: function() {
+    },
+    
+    create: function() {
+        game.stage.backgroundColor = "#000";
+        
+        player = game.add.sprite(200, game.world.centerY, 'playerCollision').anchor.set(0.5);
+        //game.add.sprite();
+        
+        cursors = game.input.keyboard.createCursorKeys();
+        fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    },
+    
+    update: function() {
+        if (cursors.left.isDown)
+        {
+            player.body.velocity.x -= 8;
+        }
+        else if (cursors.right.isDown)
+        {
+            player.velocity.x += 8;
+        }
+
+        if (cursors.up.isDown)
+        {
+            player.y -= 8;
+        }
+        else if (cursors.down.isDown)
+        {
+            player.y += 8;
+        }
+
+        /*if (fireButton.isDown)
+        {
+            fireBullet();
+        }*/
+
+        //lazers.forEachAlive(updateBullets, this);
+    }
+};
--- hardcore-pilot/js/main.js
+++ hardcore-pilot/js/main.js
@@ -3,6 +3,9 @@ var game = new Phaser.Game(config.screenWidth, config.screenHeight, config.rende
 game.state.add('boot', state1);
 game.state.add('intro', state2);
 game.state.add('load', state3);
+//game.state.add('menu', state4);
+game.state.add('loadassets', state5);
+game.state.add('play', state6);
 
 game.state.start('boot');
 console.log('work');
